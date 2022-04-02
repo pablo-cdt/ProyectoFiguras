9ce647dfb66ca74ce12fe70fbaf6e0656f47e171
Pablo Alfredo Castañeda del Toro <paps.cdt@gmail.com>
2022-04-02 12:55:29 -0600

    Fixed multiple frames issue
    fixed an issue where when using multiple threads to open a JOptionPane, it would only focus on one window

--- src/services/GuardarCalculosService.java
+++ src/services/GuardarCalculosService.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 
 public class GuardarCalculosService implements Runnable{
     private static IMedidas figura;
-    private static JOptionPane pane = new JOptionPane();
     GuardarCalculosService(IMedidas figura){
         this.figura = figura;
     }
@@ -23,34 +22,38 @@ public class GuardarCalculosService implements Runnable{
 
     private static void escribirNuevoArchivo(String nombreArchivo, IMedidas figuraElegida) {
         try {
+
             PrintWriter printWriter = new PrintWriter(nombreArchivo);
             printWriter.println(figuraElegida.toString());
             printWriter.close();
         }catch (Exception e){
         }
-
-        JOptionPane.showMessageDialog(pane,Mensajes.ARCHIVO_GENERADO);
+        JFrame frame = new JFrame();
+        frame.requestFocus();
+        JOptionPane.showMessageDialog(frame,Mensajes.ARCHIVO_GENERADO);
     }
 
     public static String crearNuevoArchivo() {
 
+        JFrame frame = new JFrame();
+        frame.requestFocus();
         LocalDate todaysDate = LocalDate.now();
         boolean nombreIngresadoCorrectamente = false;
         String nombreArchivo;
         String rutaArchivo = "";
         try {
             do {
-                nombreArchivo = JOptionPane.showInputDialog(pane,Mensajes.PEDIR_NOMBRE_ARCHIVO);
+                nombreArchivo = JOptionPane.showInputDialog(frame,Mensajes.PEDIR_NOMBRE_ARCHIVO);
                 rutaArchivo = String.format(Mensajes.RUTA_ARCHIVO, todaysDate, "/", nombreArchivo);
                 File archivo = new File(rutaArchivo);
                 if (archivo.exists()) {
-                    String decision = JOptionPane.showInputDialog(pane,Mensajes.CONFIRMACION_SOBREESCRITURA);
+                    String decision = JOptionPane.showInputDialog(frame,Mensajes.CONFIRMACION_SOBREESCRITURA);
                     if (decision.equalsIgnoreCase(Mensajes.CONFIRMACION)) {
                         archivo.createNewFile();
                         nombreIngresadoCorrectamente = true;
                     } else if (decision.equalsIgnoreCase(Mensajes.NEGACION))
-                        JOptionPane.showMessageDialog(null, "Regresando al menu anterior");
-                    else JOptionPane.showMessageDialog(null, "Ingrese una opcion valida en el menu");
+                        JOptionPane.showMessageDialog(frame, "Regresando al menu anterior");
+                    else JOptionPane.showMessageDialog(frame, "Ingrese una opcion valida en el menu");
                 } else {
                     archivo.createNewFile();
                     nombreIngresadoCorrectamente = true;
@@ -58,7 +61,7 @@ public class GuardarCalculosService implements Runnable{
 
             } while (!nombreIngresadoCorrectamente);
         }catch (IOException e) {
-            JOptionPane.showMessageDialog(null,"Error fatal al crear Archivo");
+            JOptionPane.showMessageDialog(frame,"Error fatal al crear Archivo");
         }
         return rutaArchivo;
 
